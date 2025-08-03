6b1e113eb72c28252979c0b2e22a67948aa5f700
Jiwon Kwak <koagjw@gmail.com>
2025-08-03 13:14:22 +0900

    fix: 대기중, 게임중에 뒤로 가기 동작 처리 (#8)

--- src/layout/game/GameLayout.js
+++ src/layout/game/GameLayout.js
@@ -1,142 +1,211 @@
-import {Outlet, useNavigate, useParams} from "react-router-dom";
-import Sidebar from "./Sidebar";
-import {useSetRecoilState} from "recoil";
+import { Outlet, useNavigate, useParams } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
 import {
-    chatAtom, gameResultAtom,
-    gameSettingAtom,
-    loginUserAtom,
-    playerListAtom,
-    questionResultAtom,
-    questionsAtom,
-    questionStartAtom,
-    rankUpdateAtom,
-    roomSettingAtom,
-    stompSendMessageAtom,
-    systemNoticeAtom
-} from "../../state/atoms";
-import useStompClient from "../../hooks/useStompClient";
-import {useCallback, useEffect} from "react";
-import {useApiQuery} from "../../hooks/useApiQuery";
-import axios from "axios";
+  chatAtom,
+  gameResultAtom,
+  gameSettingAtom,
+  loginUserAtom,
+  playerListAtom,
+  questionResultAtom,
+  questionsAtom,
+  questionStartAtom,
+  rankUpdateAtom,
+  roomSettingAtom,
+  stompSendMessageAtom,
+  systemNoticeAtom,
+  quizStartedAtom,
+} from '../../state/atoms';
+import useStompClient from '../../hooks/useStompClient';
+import { useCallback, useEffect, useRef, useLayoutEffect } from 'react';
+import { useApiQuery } from '../../hooks/useApiQuery';
+import axios from 'axios';
 
 const PLAYER_COLORS = [
-    "text-red-600",
-    "text-blue-600",
-    "text-red-500",
-    "text-orange-500",
-    "text-cyan-500",
-    "text-green-600",
-    "text-purple-600",
-    "text-pink-600",
+  'text-red-600',
+  'text-blue-600',
+  'text-red-500',
+  'text-orange-500',
+  'text-cyan-500',
+  'text-green-600',
+  'text-purple-600',
+  'text-pink-600',
 ];
 
 const authMeRequest = async () => {
-    const response = await axios.get(`/auth/me`);
-    return response.data;
+  const response = await axios.get(`/auth/me`);
+  return response.data;
 };
 
 const GameLayout = () => {
-    const { id: roomId } = useParams();
-    const setPlayerList = useSetRecoilState(playerListAtom);
-    const setRoomSetting = useSetRecoilState(roomSettingAtom);
-    const setGameSetting = useSetRecoilState(gameSettingAtom);
-    const setChat = useSetRecoilState(chatAtom);
-    const setQuestions = useSetRecoilState(questionsAtom);
-    const setQuestionStart = useSetRecoilState(questionStartAtom);
-    const setQuestionResult = useSetRecoilState(questionResultAtom);
-    const setRankUpdate = useSetRecoilState(rankUpdateAtom);
-    const setGameResult = useSetRecoilState(gameResultAtom);
-    const setSystemNotice = useSetRecoilState(systemNoticeAtom);
-    const setSendMessage = useSetRecoilState(stompSendMessageAtom);
-    const setLoginUser = useSetRecoilState(loginUserAtom);
-    const navigate = useNavigate();
+  const { id: roomId } = useParams();
+  const setPlayerList = useSetRecoilState(playerListAtom);
+  const setRoomSetting = useSetRecoilState(roomSettingAtom);
+  const setGameSetting = useSetRecoilState(gameSettingAtom);
+  const setChat = useSetRecoilState(chatAtom);
+  const setQuestions = useSetRecoilState(questionsAtom);
+  const setQuestionStart = useSetRecoilState(questionStartAtom);
+  const setQuestionResult = useSetRecoilState(questionResultAtom);
+  const setRankUpdate = useSetRecoilState(rankUpdateAtom);
+  const setGameResult = useSetRecoilState(gameResultAtom);
+  const setSystemNotice = useSetRecoilState(systemNoticeAtom);
+  const setSendMessage = useSetRecoilState(stompSendMessageAtom);
+  const setLoginUser = useSetRecoilState(loginUserAtom);
+  const navigate = useNavigate();
 
-    const { isLoading, data } = useApiQuery(
-        ["authme"],
-        () => authMeRequest(),
-    );
+  const isQuizStarted = useRecoilValue(quizStartedAtom);
 
-    useEffect(() => {
-        if (data) {
-            setLoginUser(data);
+  const { isLoading, data } = useApiQuery(['authme'], () => authMeRequest());
+
+  useEffect(() => {
+    if (data) {
+      setLoginUser(data);
+    }
+  }, [data, setLoginUser]);
+
+  const disconnectRef = useRef(null);
+  const ignorePopState = useRef(false);
+
+  const handleStompMessage = useCallback(
+    (payload) => {
+      console.log('receive message: ', payload);
+      switch (payload.type) {
+        case 'PLAYER_LIST':
+          const { host, players } = payload.message;
+          const processedPlayers = players.map((player, index) => {
+            let status = 'waiting';
+            if (player.nickname === host) {
+              status = 'host';
+            } else if (player.ready) {
+              status = 'ready';
+            }
+            return {
+              ...player,
+              status,
+              color: PLAYER_COLORS[index] || 'text-gray-500',
+            };
+          });
+          setPlayerList(processedPlayers);
+          break;
+        case 'ROOM_SETTING':
+          setRoomSetting(payload.message);
+          break;
+        case 'GAME_SETTING':
+          setGameSetting(payload.message);
+          break;
+        case 'SYSTEM_NOTICE':
+          setSystemNotice(payload.message);
+          break;
+        case 'CHAT':
+          setChat(payload.message);
+          break;
+        case 'GAME_START':
+          setQuestions(payload.message.questions);
+          navigate('play');
+          break;
+        case 'QUESTION_START':
+          setQuestionStart(payload.message);
+          break;
+        case 'QUESTION_RESULT':
+          setQuestionResult(payload.message);
+          break;
+        case 'RANK_UPDATE':
+          setRankUpdate(payload.message.rank);
+          break;
+        case 'GAME_RESULT':
+          setGameResult(payload.message.result);
+          break;
+        case 'EXIT_SUCCESS':
+          // 서버로부터 퇴장 성공 메시지를 받은 후 소켓 연결을 끊고 페이지 이동
+          if (disconnectRef.current) {
+            disconnectRef.current();
+          }
+          navigate('/room');
+          break;
+        default:
+          console.warn('알 수 없는 메시지', payload);
+      }
+    },
+    [
+      setPlayerList,
+      setRoomSetting,
+      setGameSetting,
+      setSystemNotice,
+      setChat,
+      setQuestions,
+      setQuestionStart,
+      setQuestionResult,
+      setRankUpdate,
+      setGameResult,
+      navigate,
+    ],
+  );
+
+  const { sendMessage, disconnect } = useStompClient(
+    roomId,
+    handleStompMessage,
+  );
+
+  useEffect(() => {
+    disconnectRef.current = disconnect;
+  }, [disconnect]);
+
+  useEffect(() => {
+    if (sendMessage) {
+      setSendMessage(() => sendMessage);
+    }
+  }, [sendMessage, setSendMessage]);
+
+  // **뒤로가기 이벤트를 감지하고 라우팅을 방지하는 로직**
+  useLayoutEffect(() => {
+    const handlePopState = (event) => {
+      // ignorePopState 플래그가 true이면 이벤트를 무시하고 플래그를 false로 되돌립니다.
+      if (ignorePopState.current) {
+        ignorePopState.current = false;
+        return;
+      }
+
+      if (isQuizStarted) {
+        const userConfirmed = window.confirm(
+          '지금 나가시면 게임 결과가 반영되지 않습니다. 정말 나가시겠습니까?',
+        );
+        if (userConfirmed) {
+          // '확인'을 누르면 서버에 퇴장 메시지를 보냅니다.
+          if (disconnectRef.current) {
+            disconnectRef.current();
+            navigate('/room');
+          }
+        } else {
+          // '취소'를 누르면 뒤로가기 동작을 무효화하고 현재 페이지에 머무릅니다.
+          // history.go(1)을 통해 뒤로가기 기록을 앞으로 이동시키고,
+          // ignorePopState 플래그를 설정하여 불필요한 이벤트 중복을 막습니다.
+          ignorePopState.current = true;
+          window.history.go(1);
         }
-    }, [data])
-
-    // 메시지를 처리하는 콜백
-    const handleStompMessage = useCallback((payload) => {
-        console.log('receive message: ', payload)
-        switch (payload.type) {
-            case "PLAYER_LIST":
-                const { host, players } = payload.message;
-                const processedPlayers = players.map((player, index) => {
-                    let status = "waiting";
-                    if (player.nickname === host) {
-                        status = "host";
-                    } else if (player.ready) {
-                        status = "ready";
-                    }
-                    return {
-                        ...player,
-                        status,
-                        color: PLAYER_COLORS[index] || "text-gray-500",
-                    };
-                });
-                setPlayerList(processedPlayers);
-                break;
-            case "ROOM_SETTING":
-                setRoomSetting(payload.message);
-                break;
-            case "GAME_SETTING":
-                setGameSetting(payload.message);
-                break;
-            case "SYSTEM_NOTICE":
-                setSystemNotice(payload.message);
-                break;
-            case "CHAT":
-                setChat(payload.message);
-                break;
-            case "GAME_START":
-                setQuestions(payload.message.questions);
-                navigate("play");
-                break;
-            case "QUESTION_START":
-                setQuestionStart(payload.message);
-                break;
-            case "QUESTION_RESULT":
-                setQuestionResult(payload.message);
-                break;
-            case "RANK_UPDATE":
-                setRankUpdate(payload.message.rank);
-                break;
-            case "GAME_RESULT":
-                setGameResult(payload.message.result);
-                break;
-            case "EXIT_SUCCESS":
-                disconnect();
-                navigate("/room");
-                break;
-            default:
-                console.warn("알 수 없는 메시지", payload);
-        }
-    }, [setPlayerList, setRoomSetting, setGameSetting, setSystemNotice, setChat]);
-
-    const { sendMessage, disconnect } = useStompClient(roomId, handleStompMessage);
-
-    useEffect(() => {
+      } else {
+        // 게임 시작 전에는 경고 없이 바로 퇴장 메시지를 보냅니다.
         if (sendMessage) {
-            setSendMessage(() => sendMessage); // Recoil 전역 등록
+          sendMessage(`/pub/room/exit/${roomId}`, '');
         }
-    }, [sendMessage]);
+      }
+    };
 
-    return (
-        <div className="flex h-screen">
-            <Sidebar />
-            <main className="flex-1 overflow-y-auto">
-                <Outlet />
-            </main>
-        </div>
+    window.history.pushState(null, null, window.location.href);
+    window.addEventListener('popstate', handlePopState);
 
-    );
-}
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [sendMessage, roomId, isQuizStarted, disconnectRef]);
+
+  return (
+    <div className='flex h-screen'>
+      <Sidebar />
+      <main className='flex-1 overflow-y-auto'>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
 export default GameLayout;
--- src/pages/game/GamePlay.js
+++ src/pages/game/GamePlay.js
@@ -1,131 +1,168 @@
-import {useEffect, useState} from "react"
-import {useNavigate, useParams} from "react-router-dom"
-import F1StartingLights from "../../layout/game/components/F1StartingLights"
-import QuizTimer from "../../layout/game/components/QuizTimer"
-import QuizQuestion from "../../layout/game/components/QuizQuestion"
-import QuizResultsModal from "../../layout/game/components/QuizResultsModal"
-import {useRecoilState, useRecoilValue} from "recoil";
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import F1StartingLights from '../../layout/game/components/F1StartingLights';
+import QuizTimer from '../../layout/game/components/QuizTimer';
+import QuizQuestion from '../../layout/game/components/QuizQuestion';
+import QuizResultsModal from '../../layout/game/components/QuizResultsModal';
+import { useRecoilState, useSetRecoilState } from 'recoil'; // useSetRecoilState 추가
 import {
-    gameResultAtom,
-    questionResultAtom,
-    questionsAtom,
-    questionStartAtom
-} from "../../state/atoms";
-import WinnerModal from "../../layout/game/components/WinnerModal";
+  gameResultAtom,
+  questionResultAtom,
+  questionsAtom,
+  questionStartAtom,
+  quizStartedAtom, // 새로 추가한 아톰
+} from '../../state/atoms';
+import WinnerModal from '../../layout/game/components/WinnerModal';
 
 function GamePlay() {
-    const { id: roomId } = useParams();
-    const navigate = useNavigate();
-    const [showStartingLights, setShowStartingLights] = useState(true);
-    const [quizStarted, setQuizStarted] = useState(false);
-    const [quizCompleted, setQuizCompleted] = useState(false);
-    const [showResultsModal, setShowResultsModal] = useState(false);
-    const [questions, setQuestions] = useRecoilState(questionsAtom);
-    const [currentQuestion, setCurrentQuestion] = useRecoilState(questionStartAtom);
-    const [questionsResult, setQuestionsResult] = useRecoilState(questionResultAtom);
-    const [gameResult, setGameResult] = useRecoilState(gameResultAtom);
-    const [visibleQuestion, setVisibleQuestion] = useState(false);
-    const [showWinnerModal, setShowWinnerModal] = useState(false);
+  const { id: roomId } = useParams();
+  const navigate = useNavigate();
+  const [showStartingLights, setShowStartingLights] = useState(true);
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [quizCompleted, setQuizCompleted] = useState(false);
+  const [showResultsModal, setShowResultsModal] = useState(false);
+  const [questions, setQuestions] = useRecoilState(questionsAtom);
+  const [currentQuestion, setCurrentQuestion] =
+    useRecoilState(questionStartAtom);
+  const [questionsResult, setQuestionsResult] =
+    useRecoilState(questionResultAtom);
+  const [gameResult, setGameResult] = useRecoilState(gameResultAtom);
+  const [visibleQuestion, setVisibleQuestion] = useState(false);
+  const [showWinnerModal, setShowWinnerModal] = useState(false);
 
-    // console.log('questions: ', questions, currentQuestion, questionsResult, );
+  // useSetRecoilState를 사용하여 quizStartedAtom을 업데이트하는 함수를 가져옵니다.
+  const setQuizStartedState = useSetRecoilState(quizStartedAtom);
 
-    const handleLightsComplete = () => {
-        setShowStartingLights(false)
-        setQuizStarted(true)
-    }
+  const handleLightsComplete = () => {
+    setShowStartingLights(false);
+    setQuizStarted(true);
+    setQuizStartedState(true); // 게임 시작 상태를 true로 설정합니다.
+  };
 
-    const handleTimeUp = () => {
-    }
+  const handleTimeUp = () => {};
 
-    const handleCloseWinnerModal = () => {
+  const handleCloseWinnerModal = () => {
+    setShowWinnerModal(false);
+  };
+
+  const handleCloseResultsModal = () => {
+    setShowResultsModal(false);
+    setQuizCompleted(true);
+  };
+
+  // QUESTION_START 메시지를 받으면 정해진 timestamp에 퀴즈 렌더링
+  useEffect(() => {
+    if (currentQuestion) {
+      const trimmed = currentQuestion.timestamp.replace(/\.\d+Z$/, 'Z');
+      const targetTime = new Date(trimmed);
+      const now = new Date();
+      const delay = targetTime.getTime() - now.getTime();
+
+      if (delay <= 0) {
+        setVisibleQuestion(true);
         setShowWinnerModal(false);
+      } else {
+        const timer = setTimeout(() => {
+          setVisibleQuestion(true);
+          setShowWinnerModal(false);
+        }, delay);
+        return () => clearTimeout(timer);
+      }
     }
+  }, [currentQuestion]);
 
-    const handleCloseResultsModal = () => {
-        setShowResultsModal(false);
-        setQuizCompleted(true);
+  useEffect(() => {
+    if (questionsResult) {
+      setVisibleQuestion(false);
+      setShowWinnerModal(true);
     }
+  }, [questionsResult]);
 
-    //QUESTION_START 메시지를 받으면 정해진 timestamp에 퀴즈 렌더링
-    useEffect(() => {
-        if (currentQuestion) {
-            const trimmed = currentQuestion.timestamp.replace(/\.\d+Z$/, 'Z');
-            const targetTime = new Date(trimmed);
+  useEffect(() => {
+    if (quizCompleted) {
+      setCurrentQuestion(null);
+      setGameResult(null);
+      setQuestions(null);
+      setQuestionsResult(null);
+      setQuizStartedState(false); // 게임 완료 후에는 상태를 false로 변경합니다.
+      navigate(`/room/${roomId}`);
+    }
+  }, [
+    quizCompleted,
+    navigate,
+    roomId,
+    setCurrentQuestion,
+    setGameResult,
+    setQuestions,
+    setQuestionsResult,
+    setQuizStartedState,
+  ]);
 
-            const now = new Date();
-            const delay = targetTime.getTime() - now.getTime(); // 밀리초 차이
+  useEffect(() => {
+    if (gameResult) {
+      const timeout = setTimeout(() => {
+        setShowWinnerModal(false);
+        setShowResultsModal(true);
+      }, 3000);
+      return () => clearTimeout(timeout);
+    }
+  }, [gameResult]);
 
-            if (delay <= 0) {
-                // 이미 시간이 지났으면 바로 렌더링
-                setVisibleQuestion(true);
-                setShowWinnerModal(false);
-            } else {
-                // 아직 안 됐으면, 그 시간에 맞춰 타이머 설정
-                const timer = setTimeout(() => {
-                    setVisibleQuestion(true);
-                    setShowWinnerModal(false);
-                }, delay);
+  // 이 컴포넌트 내부에 있던 뒤로가기 로직은 삭제되었습니다.
+  // 이제 뒤로가기 이벤트는 GameLayout에서 전역 상태(quizStartedAtom)를 기반으로 처리합니다.
 
-                return () => clearTimeout(timer); // 언마운트 시 정리
-            }
-        }
-    }, [currentQuestion]);
-
-    useEffect(() => {
-        if (questionsResult) {
-            setVisibleQuestion(false);
-            setShowWinnerModal(true);
-        }
-    }, [questionsResult]);
-
-    useEffect(() => {
-        if (quizCompleted) {
-            setCurrentQuestion(null);
-            setGameResult(null);
-            setQuestions(null);
-            setQuestionsResult(null);
-
-            navigate(`/room/${roomId}`);
-        }
-    }, [quizCompleted])
-
-    useEffect(() => {
-        if (gameResult) {
-            const timeout = setTimeout(() => {
-                setShowWinnerModal(false);
-                setShowResultsModal(true);
-            }, 3000); // 3초 (3000ms) 후 실행
-
-            return () => clearTimeout(timeout);
-        }
-    }, [gameResult]);
-
-    return (
-        <div className="p-8 flex flex-col h-full">
-            {showStartingLights && <F1StartingLights onComplete={handleLightsComplete} />}
-
-            <div className={`transition-opacity duration-500 ${quizStarted ? "opacity-100" : "opacity-0"} h-full flex flex-col`}>
-                {/* Quiz Header */}
-                <div className="flex items-center justify-between mb-8">
-                    <div className="bg-white rounded-lg px-4 py-2 shadow-sm">
-                        <span className="text-lg text-gray-600">문제</span>
-                        <span className="text-lg font-bold text-gray-900 ml-2">
-                            {questions && currentQuestion && `${currentQuestion.round} / ${questions.length}`}
-                        </span>
-                    </div>
-                    {visibleQuestion && <QuizTimer duration={currentQuestion.timeLimit} onTimeUp={handleTimeUp}/>}
-                </div>
-
-                {/* Question Content */}
-                <div className="flex-1 flex justify-center">
-                    {/*<QuizCompleted totalQuestions={questions?.length} />*/}
-                    {visibleQuestion && <QuizQuestion questionContent={currentQuestion && questions && questions[currentQuestion.round - 1].question}/>}
-                </div>
-            </div>
-            <WinnerModal isVisible={showWinnerModal} questionsResult={questionsResult} onClose={handleCloseWinnerModal}/>
-            <QuizResultsModal isVisible={showResultsModal} results={gameResult ?? []} onClose={handleCloseResultsModal} />
+  return (
+    <div className='p-8 flex flex-col h-full'>
+      {showStartingLights && (
+        <F1StartingLights onComplete={handleLightsComplete} />
+      )}
+      <div
+        className={`transition-opacity duration-500 ${
+          quizStarted ? 'opacity-100' : 'opacity-0'
+        } h-full flex flex-col`}
+      >
+        {/* Quiz Header */}
+        <div className='flex items-center justify-between mb-8'>
+          <div className='bg-white rounded-lg px-4 py-2 shadow-sm'>
+            <span className='text-lg text-gray-600'>문제</span>
+            <span className='text-lg font-bold text-gray-900 ml-2'>
+              {questions &&
+                currentQuestion &&
+                `${currentQuestion.round} / ${questions.length}`}
+            </span>
+          </div>
+          {visibleQuestion && (
+            <QuizTimer
+              duration={currentQuestion.timeLimit}
+              onTimeUp={handleTimeUp}
+            />
+          )}
         </div>
-    )
+        {/* Question Content */}
+        <div className='flex-1 flex justify-center'>
+          {visibleQuestion && (
+            <QuizQuestion
+              questionContent={
+                currentQuestion &&
+                questions &&
+                questions[currentQuestion.round - 1].question
+              }
+            />
+          )}
+        </div>
+      </div>
+      <WinnerModal
+        isVisible={showWinnerModal}
+        questionsResult={questionsResult}
+        onClose={handleCloseWinnerModal}
+      />
+      <QuizResultsModal
+        isVisible={showResultsModal}
+        results={gameResult ?? []}
+        onClose={handleCloseResultsModal}
+      />
+    </div>
+  );
 }
 
 export default GamePlay;
