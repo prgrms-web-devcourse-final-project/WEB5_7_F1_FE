8549a78930c23ebdf7f05f9ce71b563538dc8960
jiwon1217 <koagjw@gmail.com>
2025-08-03 13:13:02 +0900

    fix: 대기중, 게임중에 뒤로 가기 동작 처리

--- src/state/atoms.js
+++ src/state/atoms.js
@@ -1,83 +1,88 @@
-import {atom} from "recoil";
+import { atom } from 'recoil';
 
 export const toastAtom = atom({
-    key: 'toastAtom', // 고유 키
-    default: {
-        isOpen: false,
-        message: '',
-        type : ''
-    },
+  key: 'toastAtom', // 고유 키
+  default: {
+    isOpen: false,
+    message: '',
+    type: '',
+  },
 });
 
 export const confirmAtom = atom({
-    key: 'confirmAtom',
-    default: {
-        isOpen: false // 모달 열림 여부
-        , title: 'modal title' // 모달 타이틀
-        , html: 'modal content' //모달 설명
-        , icon: 'modal-icon' //모달 아이콘
-        , confirmButtonText: '확인' //확인버튼 이름
-        , callback: null //'확인' 버튼 클릭 시 콜백 함수, close메소드는 자동 호출한다.
-        , onCancel : null
-    },
+  key: 'confirmAtom',
+  default: {
+    isOpen: false, // 모달 열림 여부
+    title: 'modal title', // 모달 타이틀
+    html: 'modal content', //모달 설명
+    icon: 'modal-icon', //모달 아이콘
+    confirmButtonText: '확인', //확인버튼 이름
+    callback: null, //'확인' 버튼 클릭 시 콜백 함수, close메소드는 자동 호출한다.
+    onCancel: null,
+  },
 });
 
 export const loginUserAtom = atom({
-    key: 'loginUser',
-    default: null,
+  key: 'loginUser',
+  default: null,
 });
 
 export const roomSettingAtom = atom({
-    key: 'roomSetting',
-    default: null,
+  key: 'roomSetting',
+  default: null,
 });
 
 export const playerListAtom = atom({
-    key: 'playerList',
-    default: [],
+  key: 'playerList',
+  default: [],
 });
 
 export const gameSettingAtom = atom({
-    key: 'gameSetting',
-    default: null,
+  key: 'gameSetting',
+  default: null,
 });
 
 export const stompSendMessageAtom = atom({
-    key: 'stompSendMessageAtom',
-    default: null, // sendMessage 함수가 들어올 예정
+  key: 'stompSendMessageAtom',
+  default: null, // sendMessage 함수가 들어올 예정
 });
 
 export const chatAtom = atom({
-    key: 'chat',
-    default: null,
+  key: 'chat',
+  default: null,
 });
 
 export const systemNoticeAtom = atom({
-    key: 'systemNotice',
-    default: null,
+  key: 'systemNotice',
+  default: null,
 });
 
 export const questionsAtom = atom({
-    key: 'questions',
-    default: null,
+  key: 'questions',
+  default: null,
 });
 
 export const questionStartAtom = atom({
-    key: 'questionStart',
-    default: null,
+  key: 'questionStart',
+  default: null,
 });
 
 export const questionResultAtom = atom({
-    key: 'questionResult',
-    default: null,
+  key: 'questionResult',
+  default: null,
 });
 
 export const rankUpdateAtom = atom({
-    key: 'rankUpdate',
-    default: null,
+  key: 'rankUpdate',
+  default: null,
 });
 
 export const gameResultAtom = atom({
-    key: 'gameResult',
-    default: null,
+  key: 'gameResult',
+  default: null,
+});
+
+export const quizStartedAtom = atom({
+  key: 'quizStartedAtom',
+  default: false,
 });
